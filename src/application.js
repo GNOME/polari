GNOME/polari

commit b446c1e4792777ff553232e8a3210b5610b2b399

ircParser: Use new join/leave actions

src/ircParser.js
@@ -136,21 +136,14 @@ const IrcParser = new Lang.Class({
                 if (argv.length)
                     log('Excess arguments to JOIN command: ' + argv);
 
-                let time = TP_CURRENT_TIME;
                 let account = this._room.channel.connection.get_account();
-                let req = Tp.AccountChannelRequest.new_text(account, time);
 
-                let preferredHandler = Tp.CLIENT_BUS_NAME_BASE + 'Polari';
-                req.set_target_id(Tp.HandleType.ROOM, room);
-                req.set_delegate_to_preferred_handler(true);
-                req.ensure_channel_async(preferredHandler, null, Lang.bind(this,
-                    function(req, res) {
-                        try {
-                            req.ensure_channel_finish(res);
-                        } catch(e) {
-                            logError(e, 'Failed to join channel');
-                        }
-                    }));
+                let app = Gio.Application.get_default();
+                let action = app.lookup_action('join-room');
+                action.activate(GLib.Variant.new('(ssu)',
+                                                 [ account.get_object_path(),
+                                                   room,
+                                                   TP_CURRENT_TIME ]));
                 break;
             }
             case 'KICK': {
@@ -220,16 +213,10 @@ const IrcParser = new Lang.Class({
                     argv.shift(); // first arg was a room name
                 else
                     room = this._room;
-                let reason = Tp.ChannelGroupChangeReason.NONE;
-                let message = argv.join(' ') || '';
-                room.channel.leave_async(reason, message, Lang.bind(this,
-                    function(c, res) {
-                        try {
-                            c.leave_finish(res);
-                        } catch(e) {
-                            logError(e, 'Failed to leave channel');
-                        }
-                    }));
+
+                let app = Gio.Application.get_default();
+                let action = app.lookup_action('leave-room');
+                action.activate(GLib.Variant.new('s', room.id));
                 break;
             }
             case 'QUERY': {
@@ -239,20 +226,14 @@ const IrcParser = new Lang.Class({
                     break;
                 }
 
-                let time = TP_CURRENT_TIME;
                 let account = this._room.channel.connection.get_account();
-                let req = Tp.AccountChannelRequest.new_text(account, time);
-                let preferredHandler = Tp.CLIENT_BUS_NAME_BASE + 'Polari';
-                req.set_target_id(Tp.HandleType.CONTACT, nick);
-                req.set_delegate_to_preferred_handler(true);
-                req.ensure_channel_async(preferredHandler, null, Lang.bind(this,
-                    function(req, res) {
-                        try {
-                            req.ensure_channel_finish(res);
-                        } catch(e) {
-                            logError(e, 'Failed to open private chat');
-                        }
-                    }));
+
+                let app = Gio.Application.get_default();
+                let action = app.lookup_action('join-room');
+                action.activate(GLib.Variant.new('(ssu)',
+                                                 [ account.get_object_path(),
+                                                   nick,
+                                                   TP_CURRENT_TIME ]));
                 break;
             }
             case 'QUIT': {

commit 0cac918c8dc7af031eff5684f2944c27944b3651

app: Add action for initiating a private conversation
This action works just like 'join-room', but uses Tp.HandleType.CONTACT
rather than Tp.HandleType.ROOM. Use it to fix the /query command that
was broken in commit b446c1e4792777.

src/application.js
@@ -78,6 +78,9 @@ const Application = new Lang.Class({
           { name: 'join-room',
             activate: Lang.bind(this, this._onJoinRoom),
             parameter_type: GLib.VariantType.new('(ssu)') },
+          { name: 'message-user',
+            activate: Lang.bind(this, this._onMessageUser),
+            parameter_type: GLib.VariantType.new('(ssu)') },
           { name: 'leave-room',
             activate: Lang.bind(this, this._onLeaveRoom),
             parameter_type: GLib.VariantType.new('s') },
@@ -258,6 +261,22 @@ const Application = new Lang.Class({
         account.update_parameters_vardict_async(asv, [], callback);
     },
 
+    _requestChannel: function(accountPath, targetType, targetId, time) {
+        // have this in AccountMonitor?
+        let factory = Tp.AccountManager.dup().get_factory();
+        let account = factory.ensure_account(accountPath, []);
+
+        let requestData = {
+          account: account,
+          targetHandleType: targetType,
+          targetId: targetId,
+          time: time,
+          retry: 0,
+          originalNick: account.nickname };
+
+        this._ensureChannel(requestData);
+    },
+
     _ensureChannel: function(requestData) {
         let account = requestData.account;
 
@@ -306,19 +325,14 @@ const Application = new Lang.Class({
 
     _onJoinRoom: function(action, parameter) {
         let [accountPath, channelName, time] = parameter.deep_unpack();
-        // have this in AccountMonitor?
-        let factory = Tp.AccountManager.dup().get_factory();
-        let account = factory.ensure_account(accountPath, []);
+        this._requestChannel(accountPath, Tp.HandleType.ROOM,
+                             channelName, time);
+    },
 
-        let requestData = {
-          account: account,
-          targetHandleType: Tp.HandleType.ROOM,
-          targetId: channelName,
-          time: time,
-          retry: 0,
-          originalNick: account.nickname };
-
-        this._ensureChannel(requestData);
+    _onMessageUser: function(action, parameter) {
+        let [accountPath, contactName, time] = parameter.deep_unpack();
+        this._requestChannel(accountPath, Tp.HandleType.CONTACT,
+                             contactName, time);
     },
 
     _onLeaveRoom: function(action, parameter) {

src/ircParser.js
@@ -230,7 +230,7 @@ const IrcParser = new Lang.Class({
                 let account = this._room.channel.connection.get_account();
 
                 let app = Gio.Application.get_default();
-                let action = app.lookup_action('join-room');
+                let action = app.lookup_action('message-user');
                 action.activate(GLib.Variant.new('(ssu)',
                                                  [ account.get_object_path(),
                                                    nick,