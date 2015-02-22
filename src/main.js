ec0a3a74c31a2d7eab49975ecbc7b72900634ed1
Florian Müllner <fmuellner@gnome.org>
2015-02-22 23:38:03 +0100

    main: Add a main() method
    We will start using gjs' package module, so prepare for the move by
    providing the entry method it expects.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ const Application = imports.application;
 const GLib = imports.gi.GLib;
 
 function start() {
+    main(ARGV);
+}
+
+function main(args) {
     let application = new Application.Application();
     if (GLib.getenv('POLARI_PERSIST'))
         application.hold();
-    return application.run(ARGV);
+    return application.run(args);
 }
