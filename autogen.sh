594a9ed10f1557ccc880046b262e9ae3cf251116
Florian Müllner <fmuellner@gnome.org>
2015-08-05 22:19:51 +0200

    build: Remove gnome-common dependency
    It's dead[0] Jim, it's dead ...
    
    [0] https://mail.gnome.org/archives/desktop-devel-list/2015-May/msg00040.html

--- autogen.sh
+++ autogen.sh
@@ -6,14 +6,35 @@ test -z "$srcdir" && srcdir=.
 
 (test -f $srcdir/configure.ac \
   && test -d $srcdir/src) || {
-    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo -n "*** ERROR: Directory "\`$srcdir\'" does not look like the"
     echo " top-level polari directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from GNOME Git (or from"
-    echo "your OS vendor's package manager)."
-    exit 1
-}
-. gnome-autogen.sh
+PKG_NAME=`autoconf --trace 'AC_INIT:$1' configure.ac`
+
+olddir=`pwd`
+
+if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
+    echo "*** WARNING: I am going to run \`configure' with no arguments." >&2
+    echo "*** If you wish to pass any to it, please specify them on the" >&2
+    echo "*** \`$0' command line." >&2
+    echo "" >&2
+fi
+
+cd $srcdir
+aclocal --install || exit 1
+intltoolize --force --copy --automake || exit 1
+autoreconf --verbose --force --install -Wno-portability || exit 1
+
+cd $olddir
+
+if [ "$NOCONFIGURE" = "" ]; then
+    $srcdir/configure "$@" || exit 1
+
+    if [ "$1" = "--help" ]; then exit 0 else
+        echo "Now type \`make' to compile $PKG_NAME" || exit 1
+    fi
+else
+    echo "Skipping configure process."
+fi
