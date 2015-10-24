GNOME/polari

commit 84ff117eb0666c9e437d118b66007b390f925cf7

joinDialog: Don't close when saving new account
The whole point of allowing users to create new connections directly
from the join dialog was to ease the task of joining a room; closing
the dialog on account creation and requiring the user to open the
dialog again clearly defeats that purpose.

https://bugzilla.gnome.org/show_bug.cgi?id=756702

src/joinDialog.js
@@ -50,6 +50,8 @@ const JoinDialog = new Lang.Class({
             function(w, response) {
                 if (response == Gtk.ResponseType.OK)
                     this._onConfirmClicked();
+                else
+                    this.widget.destroy();
             }));
         this.widget.connect('destroy', Lang.bind(this,
             function() {
@@ -139,6 +141,7 @@ const JoinDialog = new Lang.Class({
     _onConfirmClicked: function() {
         if (this._page == DialogPage.MAIN) {
             this._joinRoom();
+            this._widget.destroy();
         } else {
             this._details.save();
             this._setPage(DialogPage.MAIN);

src/mainWindow.js
@@ -212,20 +212,12 @@ const MainWindow = new Lang.Class({
         let dialog = new JoinDialog.JoinDialog();
         dialog.widget.transient_for = this.window;
         dialog.widget.show();
-        dialog.widget.connect('response',
-            function(widget) {
-                widget.destroy();
-            });
     },
 
     showMessageUserDialog: function() {
         let dialog = new MessageDialog.MessageDialog();
         dialog.widget.transient_for = this.window;
         dialog.widget.show();
-        dialog.widget.connect('response',
-            function(widget) {
-                widget.destroy();
-            });
     },
 
     _updateUserListLabel: function() {

src/messageDialog.js
@@ -52,6 +52,7 @@ const MessageDialog = new Lang.Class({
             function(w, response) {
                 if (response == Gtk.ResponseType.OK)
                     this._onMessageClicked();
+                this.widget.destroy();
             }));
     },
 

commit 54e88f37becd1f092ac967eb8f41b903b3fec92b

joinDialog: Fix variable name
Fallout from commit 84ff117eb.

src/joinDialog.js
@@ -145,7 +145,7 @@ const JoinDialog = new Lang.Class({
     _onConfirmClicked: function() {
         if (this._page == DialogPage.MAIN) {
             this._joinRoom();
-            this._widget.destroy();
+            this.widget.destroy();
         } else {
             this._details.save();
             this._setPage(DialogPage.MAIN);