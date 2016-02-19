83ad7de2b2672a8bfcc8ac3ea3b00c11ca60bbee
Florian Müllner <fmuellner@gnome.org>
2016-02-19 12:45:26 +0100

    app: Add --debug command line flag
    It runs polari with a different application ID and doesn't disconnect
    when closed, so it allows hacking on polari while using it for chatting
    at the same time :-)

--- src/accountsMonitor.js
+++ src/accountsMonitor.js
@@ -18,8 +18,10 @@ class AccountsMonitor {
         this._accountSettings = new Map();
 
         this._app = Gio.Application.get_default();
-        this._app.connect('prepare-shutdown',
-                          Lang.bind(this, this._onPrepareShutdown));
+
+        if (!this._app.isDebugInstance)
+            this._app.connect('prepare-shutdown',
+                              Lang.bind(this, this._onPrepareShutdown));
 
         this._accountManager = Tp.AccountManager.dup();
 
