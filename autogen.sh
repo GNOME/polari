b1cc018f7efdf3084e1fabe2dde2d2ad9a80d51d
Florian Müllner <fmuellner@gnome.org>
2016-04-22 23:33:01 +0200

    build: Say good-bye to intltool
    intltool is unmaintained nowadays while upstream gettext gained
    support for everything we used it for, so make the switch.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=765445

--- autogen.sh
+++ autogen.sh
@@ -24,7 +24,6 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
 fi
 
 aclocal --install || exit 1
-intltoolize --force --copy --automake || exit 1
 autoreconf --verbose --force --install -Wno-portability || exit 1
 
 cd $olddir
