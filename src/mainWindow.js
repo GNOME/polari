18fd551d4a712c90771f39f717ba8e31b1ab268c
Florian Müllner <fmuellner@gnome.org>
2016-07-22 03:40:51 +0200

    ui: Set up bindings in the .ui file

--- src/mainWindow.js
+++ src/mainWindow.js
@@ -102,7 +102,6 @@ const MainWindow = new Lang.Class({
     InternalChildren: ['titlebarRight',
                        'titlebarLeft',
                        'joinButton',
-                       'searchButton',
                        'searchBar',
                        'searchEntry',
                        'showUserListButton',
@@ -222,17 +221,6 @@ const MainWindow = new Lang.Class({
 
         // search start
         this._keywords = [];
-
-        this.bind_property('search-active',
-                           this._searchButton,
-                           'active',
-                           GObject.BindingFlags.SYNC_CREATE |
-                           GObject.BindingFlags.BIDIRECTIONAL);
-        this.bind_property('search-active',
-                           this._searchBar,
-                           'search-mode-enabled',
-                           GObject.BindingFlags.SYNC_CREATE |
-                           GObject.BindingFlags.BIDIRECTIONAL);
 
         this._searchBar.connect_entry(this._searchEntry);
         this._searchBar.connect('notify::search-mode-enabled',
