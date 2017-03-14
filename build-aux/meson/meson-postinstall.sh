b725582b86a28a782c5984a6de97cbf574569bd9
Patrick Griffis <tingping@tingping.se>
2017-03-13 22:12:17 -0400

    build: Meson cleanup
    - Use relative paths
    - Remove uneeded join_paths()
    - Fix post-install script handling DESTDIR

--- build-aux/meson/meson-postinstall.sh
+++ build-aux/meson/meson-postinstall.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-glib-compile-schemas ${MESON_INSTALL_PREFIX}/share/glib-2.0/schemas
-update-desktop-database -q
-gtk-update-icon-cache -q -t -f ${MESON_INSTALL_PREFIX}/share/icons/hicolor
+# Package managers set this so we don't need to run
+if [ -z "$DESTDIR" ]; then
+  glib-compile-schemas ${MESON_INSTALL_PREFIX}/share/glib-2.0/schemas
+  update-desktop-database -q ${MESON_INSTALL_PREFIX}/share/applications
+  gtk-update-icon-cache -q -t -f ${MESON_INSTALL_PREFIX}/share/icons/hicolor
+fi
