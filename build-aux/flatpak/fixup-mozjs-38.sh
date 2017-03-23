83baedc474b37084d1247f17b290e0228a228e55
Florian Müllner <fmuellner@gnome.org>
2017-03-23 18:57:27 +0100

    Revert "flatpak: Hack around ninja's 0-mtime sillyness"
    The runtime now contains a patched ninja that doesn't treat
    files with 0 mtime as dirty, so we no longer need this dirty
    workaround.
    
    This reverts commit 9e609473527c54e0de768058be9e10327b229626.

--- build-aux/flatpak/fixup-mozjs-38.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# mozjs-38.pc includes an '-include' flat that points to a header
-# ninja considers "dirty"; just kill it off.
-mozjs=/usr/lib/pkgconfig/mozjs-38.pc
-sed -e 's@-include [^ ]\+@@' $mozjs > /app${mozjs##/usr}
