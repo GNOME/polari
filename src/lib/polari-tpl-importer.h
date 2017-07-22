e83ca79462b2d01e9e07a04cc1d525b42cf8902b
Florian Müllner <fmuellner@gnome.org>
2017-07-22 20:21:39 +0200

    lib: Add PolariTplImporter
    Telepathy-logger uses GMarkup functions to escape markup when writing
    logs, so for maximum compatibility we should use the same for reading.
    
    That means the implementation has to happen in C, so add a new
    importer class that imports the contents of an XML log file
    into tracker.
    
    Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/264>

---
+++ src/lib/polari-tpl-importer.h
@@ -0,0 +1,33 @@
+/*
+ * SPDX-FileCopyrightText: 2017 Florian Müllner <fmuellner@gnome.org>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#pragma once
+
+#include <gio/gio.h>
+
+#include <libtracker-sparql/tracker-sparql.h>
+
+G_BEGIN_DECLS
+
+#define POLARI_TYPE_TPL_IMPORTER (polari_tpl_importer_get_type())
+
+G_DECLARE_FINAL_TYPE (PolariTplImporter, polari_tpl_importer, POLARI, TPL_IMPORTER, GObject)
+
+PolariTplImporter *polari_tpl_importer_new (void);
+
+void   polari_tpl_importer_import_async  (PolariTplImporter   *self,
+                                          GFile               *file,
+                                          GCancellable        *cancellable,
+                                          GAsyncReadyCallback  callback,
+                                          gpointer             user_data);
+
+TrackerBatch *polari_tpl_importer_import_finish (PolariTplImporter  *self,
+                                                 GAsyncResult       *result,
+                                                 GError            **error);
+
+#define POLARI_TYPE_TPL_MESSAGE (polari_tpl_message_get_type())
+
+G_END_DECLS
