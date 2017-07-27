12c38cf7c97405cca6f9f8345238560435569aa1
Florian Müllner <fmuellner@gnome.org>
2017-07-27 10:41:13 +0200

    lib: Remove tp-logger dependency again

--- src/lib/polari-message.c
+++ src/lib/polari-message.c
@@ -73,23 +73,6 @@ polari_message_new_from_tp_message (TpMessage *tp_message)
 }
 
 PolariMessage *
-polari_message_new_from_tpl_event (TplEvent *event)
-{
-  TplTextEvent *text_event = TPL_TEXT_EVENT (event);
-  const char *text = tpl_text_event_get_message (text_event);
-  TplEntity *sender = tpl_event_get_sender (event);
-  gint64 timestamp = tpl_event_get_timestamp (event);
-  TpChannelTextMessageType type = tpl_text_event_get_message_type (text_event);
-  gboolean is_self = tpl_entity_get_entity_type (sender) == TPL_ENTITY_SELF;
-
-  return polari_message_new (text,
-                             tpl_entity_get_alias (sender),
-                             g_date_time_new_from_unix_utc (timestamp),
-                             type == TP_CHANNEL_TEXT_MESSAGE_TYPE_ACTION,
-                             is_self);
-}
-
-PolariMessage *
 polari_message_copy (PolariMessage *self)
 {
   g_return_val_if_fail (self, NULL);
