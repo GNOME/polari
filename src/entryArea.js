GNOME/polari

commit 911b95582f1781c827b204fdc606ff3e97012ebb

entryArea: Consider nickname changes when disconnected
Depending on whether the room is currently connected, we either use
the self-contact's :alias property or the account's :nickname one
for the nick button text. Either of those properties can change, but
we currently only update the text in the former case. Start tracking
changes to the :nickname property is well to make sure we properly
update the button while disconnected.

https://bugzilla.gnome.org/show_bug.cgi?id=781161

src/entryArea.js
@@ -228,6 +228,9 @@ const EntryArea = new Lang.Class({
         this._membersChangedId =
             this._room.connect('members-changed',
                                Lang.bind(this, this._updateCompletions));
+        this._nicknameChangedId =
+            this._room.account.connect('notify::nickname',
+                                       Lang.bind(this, this._updateNick));
         this._channelChangedId =
             this._room.connect('notify::channel',
                                Lang.bind(this, this._onChannelChanged));
@@ -446,6 +449,9 @@ const EntryArea = new Lang.Class({
         if (this._selfAliasChangedId)
             this._room.channel.connection.disconnect(this._selfAliasChangedId);
         this._selfAliasChangedId = 0;
+        if (this._nicknameChangedId)
+            this._room.account.disconnect(this._nicknameChangedId);
+        this._nicknameChangedId = 0;
         if (this._channelChangedId)
             this._room.disconnect(this._channelChangedId);
         this._channelChangedId = 0;

commit 29abb01374eddd5340aa58439b553791ea7347a4

entryArea: Fix setting new nick immediately
The intended behavior when changing the nick via the popover is to
update the button label immediately, and only revert to the actually
used nick when the server doesn't react in a reasonable time (or
simply rejects the update).
However 911b95582f1 broke that, as the account's nickname property
changes right away , but isn't actually used to set the nick label
while connected. In that case the label is set from the connection's
self-contact, which will only pick up the new nick after it changed
on the server.
So to avoid resetting the newly requested nick immediately, only
consider changes to the nickname while disconnected.

src/entryArea.js
@@ -249,8 +249,10 @@ var EntryArea = GObject.registerClass({
             this._room.connect('members-changed',
                                Lang.bind(this, this._updateCompletions));
         this._nicknameChangedId =
-            this._room.account.connect('notify::nickname',
-                                       Lang.bind(this, this._updateNick));
+            this._room.account.connect('notify::nickname', () => {
+                if (!this._room.channel)
+                    this._updateNick();
+            });
         this._channelChangedId =
             this._room.connect('notify::channel',
                                Lang.bind(this, this._onChannelChanged));