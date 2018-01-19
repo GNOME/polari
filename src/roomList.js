6e9e140a53f745fcea4c16725a04e09d7a87ee43
unkemptArc99 <abhishekbhardwaj540@gmail.com>
2018-01-19 11:42:48 +0530

    Changes requested in merge request https://gitlab.gnome.org/GNOME/polari/merge_requests/31 1. Initialized the spinnerActivationTime to 0 during init. Previously it was assigned to the current time when the RoomListHeader object was initialized. But that didn't make any sense, since the spinner was not active by then (which is suggested by the variable name). 2. Also, fixed a style nit at line 326 for missing a space after 'if'.
    Original issue - https://gitlab.gnome.org/GNOME/polari/issues/21

--- src/roomList.js
+++ src/roomList.js
@@ -266,7 +266,11 @@ var RoomListHeader = GObject.registerClass({
             this._account.disconnect(presenceChangedId);
         });
 
-        this._spinnerActivationTime = GLib.get_monotonic_time();
+        /* Spinner Activation time initialized to 0.
+        *  During execution, it takes the time when 
+        *  the spinner is active.
+        */
+        this._spinnerActivationTime = 0;
     }
 
     _onDisplayNameChanged() {
@@ -323,7 +327,7 @@ var RoomListHeader = GObject.registerClass({
         else if (status == Tp.ConnectionStatus.DISCONNECTED)
             child = 'disconnected';
 
-        if(isError && this._spinner.active) {
+        if (isError && this._spinner.active) {
             let spinnerTime = GLib.get_monotonic_time() - this._spinnerActivationTime;
             if (spinnerTime < MIN_SPINNER_TIME) {
                 Mainloop.timeout_add((MIN_SPINNER_TIME - spinnerTime) / 1000, () => {
