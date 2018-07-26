43fa70c29b14ea0cc4cf03bf4d7101a425b6bf7f
Florian Müllner <fmuellner@gnome.org>
2018-07-26 13:37:30 +0200

    main: Handle non-string arguments to log()
    GLib.Variant is very strict about types, so passing anything other
    than a string to one of our log functions currently throws an error.
    While this doesn't affect "real" logging, using log(someVar) is useful
    in debugging, so allow that.
    
    https://gitlab.gnome.org/GNOME/polari/merge_requests/55

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ function _makeLogFunction(level) {
 
         let [, func, file, line] = new RegExp('(.+)?@(.+):(\\d+)').exec(caller);
         GLib.log_variant(LOG_DOMAIN, level, new GLib.Variant('a{sv}', {
-            'MESSAGE': new GLib.Variant('s', message),
+            'MESSAGE': new GLib.Variant('s', `${message}`),
             'SYSLOG_IDENTIFIER': new GLib.Variant('s', 'org.gnome.Polari'),
             'CODE_FILE': new GLib.Variant('s', file),
             'CODE_FUNC': new GLib.Variant('s', func),
