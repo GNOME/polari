b48ca69f6498b3478557fe212a222696f0ad7940
daronion <stefanosdimos.98@gmail.com>
2019-08-26 19:16:10 +0300

    thumbnailer: Special-case image URLs
    Currently we use a generic approach for creating thumbnails which works
    well for web pages but can potentially have bad results for smaller
    images, as the web view adds a lot of blank space which also appears
    in the thumbnail.
    
    To regress this, detect image URLs and load them at the desired
    thumbnail size.
    
    https://gitlab.gnome.org/GNOME/polari/issues/58

--- src/thumbnailer.js
+++ src/thumbnailer.js
@@ -80,6 +80,84 @@ let PreviewWindow = GObject.registerClass({
     }
 });
 
+let DetectWindow = GObject.registerClass({
+    Properties: {
+        'uri': GObject.ParamSpec.string(
+            'uri', 'uri', 'uri',
+            GObject.ParamFlags.READWRITE | GObject.ParamFlags.CONSTRUCT_ONLY,
+            null),
+    },
+    Signals: {
+        'detect-ready': {},
+    },
+}, class DetectWindow extends Gtk.Window {
+    _init(params) {
+        this._uri = null;
+        this._contentType = null;
+
+        super._init(params);
+
+        this._view = new WebKit2.WebView({
+            is_ephemeral: true,
+            visible: true,
+        });
+        this.add(this._view);
+
+        this._view.connect('notify::is-loading',
+            this._onLoadingChanged.bind(this));
+        this._view.load_uri(this.uri);
+    }
+
+    get uri() {
+        return this._uri;
+    }
+
+    set uri(uri) {
+        if (this._uri === uri)
+            return;
+
+        this._uri = uri;
+        this.notify('uri');
+    }
+
+    _onLoadingChanged(view) {
+        if (view.is_loading)
+            return;
+
+        GLib.timeout_add(GLib.PRIORITY_DEFAULT, 500, () => {
+            this._sniffContentType();
+            return GLib.SOURCE_REMOVE;
+        });
+    }
+
+    _sniffContentType() {
+        this._view.run_javascript('document.contentType', null, (o, res) => {
+            let contentType = '';
+            try {
+                let jsResult = this._view.run_javascript_finish(res);
+                contentType = jsResult.get_js_value().to_string();
+                log(`Thumbnailer.js : Sniffed content type ${contentType}`);
+            } catch (e) {
+                log(`Thumbnailer.js : Failed to sniff content type: ${e}`);
+            }
+
+            if (contentType.startsWith('image')) {
+                log('Thumbnailer.js : Image content');
+                this._contentType = 'image';
+            } else {
+                log('Thumbnailer.js : Other than image content.');
+                this._contentType = 'other';
+            }
+
+            this.emit('detect-ready');
+        });
+    }
+
+    getContentType() {
+        return this._contentType;
+    }
+});
+
 class App {
     constructor(url, filename) {
         this._uri = url;
@@ -89,17 +167,36 @@ class App {
     run() {
         Gtk.init(null);
 
-        let window = new PreviewWindow({
-            uri: this._uri,
-            default_width: 1200,
-            default_height: 900,
-        });
+        let detectWindow = new DetectWindow({ uri: this._uri });
+        detectWindow.connect('detect-ready', this._onContentTypeDetected.bind(this));
+        detectWindow.realize();
+
+        Gtk.main();
+    }
+
+    _onContentTypeDetected(detectWindow) {
+        let contentType = detectWindow.getContentType();
+        detectWindow.destroy();
+
+        let window;
+        log(`Thumbnailer.js : detected contentType for ${this._uri}, now snapshotting...`);
+        if (contentType === 'image') {
+            window = new PreviewWindow({
+                uri: this._uri,
+                default_width: PREVIEW_WIDTH,
+                default_height: PREVIEW_HEIGHT,
+            });
+        } else {
+            window = new PreviewWindow({
+                uri: this._uri,
+                default_width: 1200,
+                default_height: 900,
+            });
+        }
 
         window.realize();
         window.connect('snapshot-ready', this._onSnapshotReady.bind(this));
         window.connect('destroy', () => Gtk.main_quit());
-
-        Gtk.main();
     }
 
     _onSnapshotReady(window) {
@@ -114,16 +211,21 @@ class App {
         let sourceWidth = surface.getWidth();
         let sourceHeight = surface.getHeight();
 
-        let target = new Cairo.ImageSurface(Cairo.Format.ARGB32,
-            PREVIEW_WIDTH,
-            PREVIEW_HEIGHT);
+        let target;
+        if (sourceWidth > PREVIEW_WIDTH || sourceHeight > PREVIEW_HEIGHT) {
+            target = new Cairo.ImageSurface(Cairo.Format.ARGB32,
+                PREVIEW_WIDTH,
+                PREVIEW_HEIGHT);
 
-        let cr = new Cairo.Context(target);
-        cr.scale(
-            PREVIEW_WIDTH / sourceWidth,
-            PREVIEW_HEIGHT / sourceHeight);
-        cr.setSourceSurface(surface, 0, 0);
-        cr.paint();
+            let cr = new Cairo.Context(target);
+            cr.scale(
+                PREVIEW_WIDTH / sourceWidth,
+                PREVIEW_HEIGHT / sourceHeight);
+            cr.setSourceSurface(surface, 0, 0);
+            cr.paint();
+        } else {
+            target = surface;
+        }
 
         target.writeToPNG(this._filename);
         log(`Thumbnailer.js : ${this._uri} image saved as ${this._filename}`);
