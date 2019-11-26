GNOME/polari

commit a191adfb456c46f1ec8c9b58cf997f11feb12fad

cleanup: Stop using hasOwnProperty() on target object
eslint added a new rule that forbids this. We aren't calling it on
arbitrary data we received from untrustful sources, so it's not really
relevant for us. Still, we can easily replace the couple of places
where we call it.

https://gitlab.gnome.org/GNOME/polari/merge_requests/125

src/chatView.js
@@ -1128,7 +1128,7 @@ var ChatView = GObject.registerClass({
         let tags = [this._lookupTag('status')];
         let groupTag = null;
         if (grouped) {
-            if (this._statusCount.hasOwnProperty(type)) {
+            if (this._statusCount.type !== undefined) {
                 this._statusCount[type]++;
                 this._statusCount.total++;
             }

src/connections.js
@@ -480,7 +480,7 @@ var ConnectionDetails = GObject.registerClass({
         if (params.use_ssl)
             details['use-ssl'] = GLib.Variant.new('b', params.use_ssl);
 
-        let removed = Object.keys(oldDetails).filter(p => !details.hasOwnProperty(p));
+        let removed = Object.keys(oldDetails).filter(p => details[p] === undefined);
 
         return [details, removed];
     }

src/mainWindow.js
@@ -354,7 +354,7 @@ var MainWindow = GObject.registerClass({
         for (let prop in selectedRoom)
             selectedRoom[prop] = selectedRoom[prop].deep_unpack();
 
-        if (!selectedRoom.hasOwnProperty('account'))
+        if (!selectedRoom.account)
             return;
 
         let roomId = null;

src/networksManager.js
@@ -65,7 +65,7 @@ var NetworksManager = class {
     getNetworkIsFavorite(id) {
         let network = this._lookupNetwork(id);
 
-        if (network.hasOwnProperty('favorite'))
+        if (Object.prototype.hasOwnProperty.call(network, 'favorite'))
             return network['favorite'];
 
         return false;

commit 0f5f09a680b42ef366eb3537ed151b9814dbf691

chatView: Fix updating status counts
Commit a191adfb456 accidentally changed the check for a valid type
('left' or 'joined') into a check for 'type', whoops.

https://gitlab.gnome.org/GNOME/polari/issues/115

src/chatView.js
@@ -1127,7 +1127,7 @@ var ChatView = GObject.registerClass({
         let tags = [this._lookupTag('status')];
         let groupTag = null;
         if (grouped) {
-            if (this._statusCount.type !== undefined) {
+            if (this._statusCount[type] !== undefined) {
                 this._statusCount[type]++;
                 this._statusCount.total++;
             }