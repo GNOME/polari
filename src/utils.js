1c151fb4290ab702faff6fba09daab48ccd38a85
Florian Müllner <fmuellner@gnome.org>
2020-04-05 00:41:34 +0200

    util: Add helper functions to clear account/identify passwords
    https://gitlab.gnome.org/GNOME/polari/-/issues/132

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,8 @@
 /* exported isFlatpakSandbox getTpEventTime findUrls findChannels openURL
             storeAccountPassword storeIdentifyPassword
             lookupAccountPassword lookupIdentifyPassword
-            updateTerms gpaste imgurPaste */
+            updateTerms gpaste imgurPaste
+            clearAccountPassword clearIdentifyPassword */
 /*
  * Copyright (c) 2011 Red Hat, Inc.
  *
@@ -143,6 +144,26 @@ function _lookupPassword(schema, account, callback) {
             let name = account.display_name;
             log(`Failed to lookup password for account "${name}": ${e.message}`);
             callback(null);
+        }
+    });
+}
+
+function clearAccountPassword(account) {
+    _clearPassword(SECRET_SCHEMA_ACCOUNT, account);
+}
+
+function clearIdentifyPassword(account) {
+    _clearPassword(SECRET_SCHEMA_IDENTIFY, account);
+}
+
+function _clearPassword(schema, account) {
+    let attr = { 'account-id': account.get_path_suffix() };
+    Secret.password_clear(schema, attr, null, (o, res) => {
+        try {
+            Secret.password_clear_finish(res);
+        } catch (e) {
+            const name = account.display_name;
+            log(`Failed to clear password for account "${name}": ${e.message}`);
         }
     });
 }
