GNOME/polari

commit 07edc92510aa901fd614024714a34a3ae4ebb7c0

cleanup: Stop monkey-patching String.prototype
Extending the standard type with a printf-style format() function
is convenient, but dodgy. gjs now rightfully discourages that
practice, so switch to using the vprintf() function from the
Format module instead.

https://gitlab.gnome.org/GNOME/polari/-/merge_requests/211

src/ircParser.js
@@ -60,7 +60,7 @@ export default class IrcParser {
     }
 
     _createFeedbackUsage(cmd) {
-        return this._createFeedbackLabel(_('Usage: %s').format(_(knownCommands[cmd])));
+        return vprintf(this._createFeedbackLabel(_('Usage: %s'), _(knownCommands[cmd])));
     }
 
     _createFeedbackGrid(header, items) {
@@ -194,7 +194,7 @@ export default class IrcParser {
             let { channel } = this._room;
             let members = channel.group_dup_members_contacts().map(m => m.alias);
             output = this._createFeedbackGrid(
-                _('Users on %s:').format(channel.identifier), members);
+                vprintf(_('Users on %s:'), channel.identifier), members);
             break;
         }
         case 'NICK': {
@@ -310,7 +310,7 @@ export default class IrcParser {
                     [last] = info[i].field_value;
             }
         }
-        return _('User: %s - Last activity: %s').format(fn ? fn : user.alias, Utils.formatTimePassed(last));
+        return vprintf(_('User: %s - Last activity: %s'), fn ? fn : user.alias, Utils.formatTimePassed(last));
     }
 
     _sendText(text) {

lint/eslintrc-polari.yml
@@ -18,5 +18,6 @@ globals:
   C_: readonly
   N_: readonly
   ngettext: readonly
+  vprintf: readonly
 parserOptions:
   sourceType: module

src/application.js
@@ -761,7 +761,7 @@ export default GObject.registerClass({
         await account.set_enabled_async(false);
         account.visible = false;
 
-        const label = _('%s removed.').format(account.display_name);
+        const label = vprintf(_('%s removed.'), account.display_name);
         const n = new AppNotifications.UndoNotification(label);
         this.notificationQueue.addNotification(n);
 

src/chatView.js
@@ -987,12 +987,12 @@ export default GObject.registerClass({
     }
 
     _onMemberRenamed(room, oldMember, newMember) {
-        let text = _('%s is now known as %s').format(oldMember.alias, newMember.alias);
+        let text = vprintf(_('%s is now known as %s'), oldMember.alias, newMember.alias);
         this._insertStatus(text, oldMember.alias, 'renamed');
     }
 
     _onMemberDisconnected(room, member, message) {
-        let text = _('%s has disconnected').format(member.alias);
+        let text = vprintf(_('%s has disconnected'), member.alias);
         if (message)
             text += ` (${message})`;
         this._insertStatus(text, member.alias, 'left');
@@ -1001,26 +1001,26 @@ export default GObject.registerClass({
     _onMemberKicked(room, member, actor) {
         let [kicked, kicker] = [member.alias, actor ? actor.alias : null];
         let msg = kicker
-            ? _('%s has been kicked by %s').format(kicked, kicker)
-            : _('%s has been kicked').format(kicked);
+            ? vprintf(_('%s has been kicked by %s'), kicked, kicker)
+            : vprintf(_('%s has been kicked'), kicked);
         this._insertStatus(msg, kicked, 'left');
     }
 
     _onMemberBanned(room, member, actor) {
         let [banned, banner] = [member.alias, actor ? actor.alias : null];
         let msg = banner
-            ? _('%s has been banned by %s').format(banned, banner)
-            : _('%s has been banned').format(banned);
+            ? vprintf(_('%s has been banned by %s'), banned, banner)
+            : vprintf(_('%s has been banned'), banned);
         this._insertStatus(msg, banned, 'left');
     }
 
     _onMemberJoined(room, member) {
-        let text = _('%s joined').format(member.alias);
+        let text = vprintf(_('%s joined'), member.alias);
         this._insertStatus(text, member.alias, 'joined');
     }
 
     _onMemberLeft(room, member, message) {
-        let text = _('%s left').format(member.alias);
+        let text = vprintf(_('%s left'), member.alias);
 
         if (message)
             text += ` (${message})`;
@@ -1115,18 +1115,18 @@ export default GObject.registerClass({
 
         let stats = [];
         if (this._statusCount.joined > 0) {
-            stats.push(
+            stats.push(vprintf(
                 ngettext(
                     '%d user joined',
-                    '%d users joined', this._statusCount.joined)
-                .format(this._statusCount.joined));
+                    '%d users joined', this._statusCount.joined),
+                this._statusCount.joined));
         }
         if (this._statusCount.left > 0) {
-            stats.push(
+            stats.push(vprintf(
                 ngettext(
                     '%d user left',
-                    '%d users left', this._statusCount.left)
-                .format(this._statusCount.left));
+                    '%d users left', this._statusCount.left),
+                this._statusCount.left));
         }
         // TODO: How do we update the arrow direction when text direction change?
         let iter = buffer.get_iter_at_mark(headerMark);

src/connections.js
@@ -513,7 +513,7 @@ export const ConnectionProperties = GObject.registerClass({
     _init(account) {
         /* Translators: %s is a connection name */
         super._init({
-            title: _('“%s” Properties').format(account.display_name),
+            title: vprintf(_('“%s” Properties'), account.display_name),
             use_header_bar: 1,
         });
 

src/entryArea.js
@@ -371,14 +371,16 @@ export default GObject.registerClass({
     }
 
     pasteText(text, nLines) {
-        this._confirmLabel.label = ngettext(
-            'Paste %s line of text to public paste service?',
-            'Paste %s lines of text to public paste service?', nLines)
-        .format(nLines);
-        this._uploadLabel.label = ngettext(
-            'Uploading %s line of text to public paste service…',
-            'Uploading %s lines of text to public paste service…', nLines)
-        .format(nLines);
+        this._confirmLabel.label = vprintf(
+            ngettext(
+                'Paste %s line of text to public paste service?',
+                'Paste %s lines of text to public paste service?', nLines),
+            nLines);
+        this._uploadLabel.label = vprintf(
+            ngettext(
+                'Uploading %s line of text to public paste service…',
+                'Uploading %s lines of text to public paste service…', nLines),
+            nLines);
         this._setPasteContent(text);
     }
 
@@ -401,9 +403,9 @@ export default GObject.registerClass({
 
         let name = fileInfo.get_display_name();
         /* Translators: %s is a filename */
-        this._confirmLabel.label = _('Upload “%s” to public paste service?').format(name);
+        this._confirmLabel.label = vprintf(_('Upload “%s” to public paste service?'), name);
         /* Translators: %s is a filename */
-        this._uploadLabel.label = _('Uploading “%s” to public paste service…').format(name);
+        this._uploadLabel.label = vprintf(_('Uploading “%s” to public paste service…'), name);
         this._setPasteContent(file);
     }
 
@@ -412,9 +414,9 @@ export default GObject.registerClass({
         let nick = this._room.channel.connection.self_contact.alias;
         if (this._room.type === Tp.HandleType.ROOM)
             /* translators: %s is a nick, #%s a channel */
-            title = _('%s in #%s').format(nick, this._room.display_name);
+            title = vprintf(_('%s in #%s'), nick, this._room.display_name);
         else
-            title = _('Paste from %s').format(nick);
+            title = vprintf(_('Paste from %s'), nick);
         this._confirmLabel.hide();
 
         this._confirmLabel.hide();

src/main.js
@@ -12,9 +12,11 @@ imports.package.init({
     libdir: Config.LIBDIR,
 });
 
-pkg.initFormat();
 pkg.initGettext();
 globalThis.ngettext = ngettext;
+
+// eslint-disable-next-line no-restricted-properties
+globalThis.vprintf = (fmt, ...args) => imports.format.vprintf(fmt, args);
 
 pkg.require({
     'GdkPixbuf': '2.0',

src/mainWindow.js
@@ -397,9 +397,9 @@ export default GObject.registerClass({
             this._room.channel.has_interface(Tp.IFACE_CHANNEL_INTERFACE_GROUP))
             numMembers = this._room.channel.group_dup_members_contacts().length;
 
-        let accessibleName = ngettext(
+        let accessibleName = vprintf(ngettext(
             '%d user',
-            '%d users', numMembers).format(numMembers);
+            '%d users', numMembers), numMembers);
         this._showUserListButton.get_accessible().set_name(accessibleName);
         this._showUserListButton.label = `${numMembers}`;
     }

commit b6839383bfe80b49ffc525377d921abfece529b7

ircParser: Fix /help
Eeeks, some fallout from commit 07edc92510aa ...

https://gitlab.gnome.org/GNOME/polari/-/merge_requests/216

src/ircParser.js
@@ -60,7 +60,7 @@ export default class IrcParser {
     }
 
     _createFeedbackUsage(cmd) {
-        return vprintf(this._createFeedbackLabel(_('Usage: %s'), _(knownCommands[cmd])));
+        return this._createFeedbackLabel(vprintf(_('Usage: %s'), _(knownCommands[cmd])));
     }
 
     _createFeedbackGrid(header, items) {