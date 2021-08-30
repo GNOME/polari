b3840f7570d92da67c518e50c47046f621c5930f
Florian Müllner <fmuellner@gnome.org>
2021-08-31 00:59:33 +0200

    build: Also check NEWS for version
    Now that we added a check-version script, extend it to also allow
    for checking the NEWS file for the current version.
    
    Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/220>

--- meson/check-version.py
+++ meson/check-version.py
@@ -4,11 +4,21 @@ import os, sys
 from pathlib import Path
 import argparse, subprocess
 
-def check_version(version, file):
-    subprocess.run(['appstream-util', 'validate-version', file, version],
-                   check=True)
+def check_version(version, file, type='news'):
+    if type == 'news':
+        line = file.open().readline()
+        ok = line.startswith(version)
+        print("{}: {}".format(file, "OK" if ok else "FAILED"))
+        if not ok:
+            raise Exception("{} does not start with {}".format(file, version))
+    elif type == 'metainfo':
+        subprocess.run(['appstream-util', 'validate-version', file, version],
+                       check=True)
+    else:
+        raise Exception('Not implemented')
 
 parser = argparse.ArgumentParser(description='Check release version information.')
+parser.add_argument('--type', choices=['metainfo','news'], default='news')
 parser.add_argument('version', help='the version to check for')
 parser.add_argument('files', nargs='+', help='files to check')
 args = parser.parse_args()
@@ -17,6 +27,6 @@ distroot = os.environ.get('MESON_DIST_ROOT', './')
 
 try:
     for file in args.files:
-        check_version(args.version, Path(distroot, file))
+        check_version(args.version, Path(distroot, file), args.type)
 except:
     sys.exit(1)
