GNOME/polari

commit 4ec0db3f1b4932b7a360ab9d507ed9233c18eb85

app: Switch to Adw.AboutWindow
libadwaita recently added an AboutDialog alternative that isn't
just adaptive, but can also be more opinionated in its design
language than GTK.

Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/256>

src/application.js
@@ -824,7 +824,7 @@ class Application extends Adw.Application {
 
     _onShowAbout() {
         let aboutParams = {
-            authors: [
+            developers: [
                 'Florian Müllner <fmuellner@gnome.org>',
                 'William Jon McCann <william.jon.mccann@gmail.com>',
                 'Carlos Soriano <carlos.soriano89@gmail.com>',
@@ -853,16 +853,16 @@ class Application extends Adw.Application {
             comments: _('An Internet Relay Chat Client for GNOME'),
             copyright: 'Copyright © 2013-2018 The Polari authors',
             license_type: Gtk.License.GPL_2_0,
-            logo_icon_name: 'org.gnome.Polari',
+            application_icon: 'org.gnome.Polari',
             version: pkg.version,
-            website_label: _('Learn more about Polari'),
+            issue_url: 'https://gitlab.gnome.org/GNOME/polari/-/issues',
             website: 'https://wiki.gnome.org/Apps/Polari',
 
             transient_for: this.active_window,
             modal: true,
         };
 
-        const aboutDialog = new Gtk.AboutDialog(aboutParams);
+        const aboutDialog = new Adw.AboutWindow(aboutParams);
         aboutDialog.show();
     }
 

commit 71a842fd621bfd42c27913e31143b9c5d80f1a7c

aboutWindow: Add back application name
We didn't set the `program-name` property of the old GtkAboutDialog,
because GTK would then fall back to the (correct) application name.

AdwAboutWindow doesn't do that, so we have to set the corresponding
property explicitly.

Fixes: 4ec0db3f1 ("app: Switch to Adw.AboutWindow")
Closes: https://gitlab.gnome.org/GNOME/polari/-/issues/210
Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/297>

src/application.js
@@ -814,6 +814,7 @@ class Application extends Adw.Application {
 
     _onShowAbout() {
         let aboutParams = {
+            application_name: _('Polari'),
             developers: [
                 'Florian Müllner <fmuellner@gnome.org>',
                 'William Jon McCann <william.jon.mccann@gmail.com>',