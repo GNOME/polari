ed854788c1dbdb52bec1c0bc888509df8b12a9fb
Florian Müllner <fmuellner@gnome.org>
2024-12-05 15:09:39 +0100

    ci: Sync install-meson-project script with gnome-shell
    While we don't need any of the fancier additions, it's better
    to not keep slightly differing copies of the same script around.
    
    Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/332>

--- .gitlab-ci/install-meson-project.sh
+++ .gitlab-ci/install-meson-project.sh
@@ -6,31 +6,106 @@
 
 set -e
 
-if [[ $# -lt 3 ]]; then
-  echo Usage: $0 [options] [repo-url] [commit] [subdir]
-  echo  Options:
-  echo    -Dkey=val
+usage() {
+  cat <<-EOF
+	Usage: $(basename $0) [OPTION…] REPO_URL COMMIT
+
+	Check out and install a meson project
+
+	Options:
+	  -Dkey=val          Option to pass on to meson
+	  --subdir=DIR       Build subdirectory instead of whole project
+	  --prepare=SCRIPT   Script to run before build
+	  --libdir=DIR       Setup the project with a different libdir
+	  --destdir=DIR      Install the project to DIR, can be used
+	                     several times to install to multiple destdirs
+
+	  -h, --help         Display this help
+
+	EOF
+}
+
+TEMP=$(getopt \
+  --name=$(basename $0) \
+  --options='D:h' \
+  --longoptions='subdir:' \
+  --longoptions='prepare:' \
+  --longoptions='libdir:' \
+  --longoptions='destdir:' \
+  --longoptions='help' \
+  -- "$@")
+
+eval set -- "$TEMP"
+unset TEMP
+
+MESON_OPTIONS=()
+SUBDIR=.
+PREPARE=:
+DESTDIRS=()
+
+while true; do
+  case "$1" in
+    -D)
+      MESON_OPTIONS+=( -D$2 )
+      shift 2
+    ;;
+
+    --subdir)
+      SUBDIR=$2
+      shift 2
+    ;;
+
+    --prepare)
+      PREPARE=$2
+      shift 2
+    ;;
+
+    --libdir)
+      MESON_OPTIONS+=( --libdir=$2 )
+      shift 2
+    ;;
+
+    --destdir)
+      DESTDIRS+=( $2 )
+      shift 2
+    ;;
+
+    -h|--help)
+      usage
+      exit 0
+    ;;
+
+    --)
+      shift
+      break
+    ;;
+  esac
+done
+
+if [[ $# -lt 2 ]]; then
+  usage
   exit 1
 fi
 
-MESON_OPTIONS=()
-
-while [[ $1 =~ ^-D ]]; do
-  MESON_OPTIONS+=( "$1" )
-  shift
-done
-
 REPO_URL="$1"
 COMMIT="$2"
-SUBDIR="$3"
 
-REPO_DIR="$(basename ${REPO_URL%.git})"
+[[ ${#DESTDIRS[@]} == 0 ]] && DESTDIRS+=( / )
 
-git clone --depth 1 "$REPO_URL" -b "$COMMIT"
-pushd "$REPO_DIR"
-pushd "$SUBDIR"
-meson --prefix=/usr _build "${MESON_OPTIONS[@]}"
-meson install -C _build
+CHECKOUT_DIR=$(mktemp --directory)
+trap "rm -rf $CHECKOUT_DIR" EXIT
+
+git clone --depth 1 "$REPO_URL" -b "$COMMIT" "$CHECKOUT_DIR"
+
+pushd "$CHECKOUT_DIR/$SUBDIR"
+sh -c "$PREPARE"
+meson setup --prefix=/usr _build "${MESON_OPTIONS[@]}"
+
+# Install it to all specified dest dirs
+for destdir in "${DESTDIRS[@]}"; do
+    # don't use --destdir when installing to root,
+    # so post-install hooks are run
+    [[ $destdir == / ]] && destdir=
+    sudo meson install -C _build ${destdir:+--destdir=$destdir}
+done
 popd
-popd
-rm -rf "$REPO_DIR"
