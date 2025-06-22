GNOME/polari

commit 33368ebac75a46d2ed053592d1d27d87903339a9

cleanup: Use regular constructors in GObject subclasses
As a side-effect of supporting class fields, regular constructors
now work in GObject subclasses. Using _init() still works and
there's no functional difference, but it's simply much nicer
to use the same syntax for all classes.

Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/243>

src/userList.js
@@ -13,8 +13,8 @@ const MAX_USERS_WIDTH_CHARS = 17;
 
 export const UserListPopover = GObject.registerClass(
 class UserListPopover extends Gtk.Popover {
-    _init(params) {
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this._createWidget();
 
@@ -131,16 +131,16 @@ export const UserDetails = GObject.registerClass({
             false),
     },
 }, class UserDetails extends Gtk.Box {
-    _init(params = {}) {
+    _expanded = false;
+    _initialDetailsLoaded = false;
+    _notificationsEnabled = false;
+    _user = null;
+
+    constructor(params = {}) {
         let { user } = params;
         delete params.user;
 
-        this._expanded = false;
-        this._initialDetailsLoaded = false;
-        this._notificationsEnabled = false;
-        this._user = null;
-
-        super._init(params);
+        super(params);
 
         this.user = user;
 
@@ -318,17 +318,18 @@ export const UserPopover = GObject.registerClass({
         'userDetails',
     ],
 }, class UserPopover extends Gtk.Popover {
-    _init(params) {
-        this._room = params.room;
-        delete params.room;
+    _nickname = null;
+    _basenick = null;
 
-        this._userTracker = params.userTracker;
+    constructor(params) {
+        const { room, userTracker } = params;
+        delete params.room;
         delete params.userTracker;
 
-        this._nickname = null;
-        this._basenick = null;
+        super(params);
 
-        super._init(params);
+        this._room = room;
+        this._userTracker = userTracker;
 
         this._app = Gio.Application.get_default();
 
@@ -422,10 +423,10 @@ export const UserPopover = GObject.registerClass({
 
 const UserListRow = GObject.registerClass(
 class UserListRow extends Gtk.ListBoxRow {
-    _init(user) {
-        this._user = user;
+    constructor(user) {
+        super({ name: `UserListRow ${user.alias}` });
 
-        super._init({ name: `UserListRow ${user.alias}` });
+        this._user = user;
 
         this._createWidget();
 

src/accountsMonitor.js
@@ -246,13 +246,12 @@ const PolariAccount = GObject.registerClass({
             Gio.Settings.$gtype),
     },
 }, class PolariAccount extends Tp.Account {
-    _init(params) {
-        this._visible = true;
-        this._reachable = undefined;
+    _visible = true;
+    _reachable = undefined;
+    _networksManager = NetworksManager.getDefault();
 
-        this._networksManager = NetworksManager.getDefault();
-
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this._settings = new Gio.Settings({
             schema_id: 'org.gnome.Polari.Account',

src/appNotifications.js
@@ -9,8 +9,8 @@ const COMMAND_OUTPUT_REVEAL_TIME = 3;
 const AppNotification = GObject.registerClass({
     GTypeFlags: GObject.TypeFlags.ABSTRACT,
 }, class AppNotification extends Gtk.Revealer {
-    _init() {
-        super._init({
+    constructor() {
+        super({
             reveal_child: true,
             transition_type: Gtk.RevealerTransitionType.SLIDE_DOWN,
         });
@@ -30,8 +30,8 @@ const AppNotification = GObject.registerClass({
 
 export const MessageNotification = GObject.registerClass(
 class MessageNotification extends AppNotification {
-    _init(label, iconName) {
-        super._init();
+    constructor(label, iconName) {
+        super();
 
         GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, TIMEOUT, this.close.bind(this));
 
@@ -75,8 +75,8 @@ export const UndoNotification = GObject.registerClass({
         undo: {},
     },
 }, class UndoNotification extends MessageNotification {
-    _init(label) {
-        super._init(label);
+    constructor(label) {
+        super(label);
 
         this._undo = false;
         this._closed = false;
@@ -99,8 +99,8 @@ export const UndoNotification = GObject.registerClass({
 const CommandOutputNotification = GObject.registerClass({
     GTypeFlags: GObject.TypeFlags.ABSTRACT,
 }, class CommandOutputNotification extends AppNotification {
-    _init() {
-        super._init();
+    constructor() {
+        super();
 
         this.transition_type = Gtk.RevealerTransitionType.SLIDE_UP;
         GLib.timeout_add_seconds(
@@ -112,8 +112,8 @@ const CommandOutputNotification = GObject.registerClass({
 
 export const SimpleOutput = GObject.registerClass(
 class SimpleOutput extends CommandOutputNotification {
-    _init(text) {
-        super._init();
+    constructor(text) {
+        super();
 
         let label = new Gtk.Label({
             label: text,
@@ -126,8 +126,8 @@ class SimpleOutput extends CommandOutputNotification {
 
 export const GridOutput = GObject.registerClass(
 class GridOutput extends CommandOutputNotification {
-    _init(header, items) {
-        super._init();
+    constructor(header, items) {
+        super();
 
         let numItems = items.length;
         let numCols = Math.min(numItems, 4);
@@ -157,8 +157,8 @@ class GridOutput extends CommandOutputNotification {
 
 export const NotificationQueue = GObject.registerClass(
 class NotificationQueue extends Gtk.Frame {
-    _init() {
-        super._init({
+    constructor() {
+        super({
             valign: Gtk.Align.START,
             halign: Gtk.Align.CENTER,
             margin_start: 24, margin_end: 24,
@@ -196,8 +196,8 @@ class NotificationQueue extends Gtk.Frame {
 
 export const CommandOutputQueue = GObject.registerClass(
 class CommandOutputQueue extends NotificationQueue {
-    _init() {
-        super._init();
+    constructor() {
+        super();
 
         this.valign = Gtk.Align.END;
         this.add_css_class('irc-feedback');
@@ -216,16 +216,16 @@ export const MessageInfoBar = GObject.registerClass({
             ''),
     },
 }, class MessageInfoBar extends Gtk.InfoBar {
-    _init(params) {
-        this._title = '';
-        this._subtitle = '';
+    _title = '';
+    _subtitle = '';
 
+    constructor(params) {
         let defaultParams = {
             show_close_button: true,
             revealed: false,
             valign: Gtk.Align.START,
         };
-        super._init(Object.assign(defaultParams, params));
+        super(Object.assign(defaultParams, params));
 
         let box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL });
         this.add_child(box);

src/application.js
@@ -43,8 +43,8 @@ export default GObject.registerClass({
         'room-focus-changed': {},
     },
 }, class Application extends Gtk.Application {
-    _init() {
-        super._init({
+    constructor() {
+        super({
             application_id: 'org.gnome.Polari',
             flags: Gio.ApplicationFlags.HANDLES_OPEN,
         });

src/chatView.js
@@ -58,11 +58,11 @@ const TextView = GObject.registerClass({
             0, GLib.MAXUINT32, 0),
     },
 }, class TextView extends Gtk.TextView {
-    _init(params) {
-        this._indentWidthChars = 0;
-        this._indentSpacing = 0;
+    _indentWidthChars = 0;
+    _indentSpacing = 0;
 
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this.buffer.connect('mark-set', this._onMarkSet.bind(this));
         this.connect('notify::root', this._onScreenChanged.bind(this));
@@ -219,10 +219,7 @@ const ButtonTag = GObject.registerClass({
         'popup-menu': { param_types: [GObject.TYPE_DOUBLE, GObject.TYPE_DOUBLE] },
     },
 }, class ButtonTag extends Gtk.TextTag {
-    _init(params) {
-        this._hover = false;
-        super._init(params);
-    }
+    _hover = false;
 
     get hover() {
         return this._hover;
@@ -257,11 +254,11 @@ const HoverFilterTag = GObject.registerClass({
             0.0, 1.0, 1.0),
     },
 }, class HoverFilterTag extends ButtonTag {
-    _init(params) {
-        this._filteredTag = null;
-        this._hoverOpacity = 1.;
+    _filteredTag = null;
+    _hoverOpacity = 1.;
 
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this.connect('notify::hover', () => this._updateColor());
     }
@@ -319,8 +316,8 @@ export default GObject.registerClass({
             0, GLib.MAXUINT32, 0),
     },
 }, class ChatView extends Gtk.ScrolledWindow {
-    _init(room) {
-        super._init({ hscrollbar_policy: Gtk.PolicyType.NEVER, vexpand: true });
+    constructor(room) {
+        super({ hscrollbar_policy: Gtk.PolicyType.NEVER, vexpand: true });
 
         this.add_css_class('polari-chat-view');
 

src/connections.js
@@ -26,15 +26,16 @@ const ErrorHint = {
 
 const ConnectionRow = GObject.registerClass(
 class ConnectionRow extends Gtk.ListBoxRow {
-    _init(params) {
+    constructor(params) {
         if (!params || !params.id)
             throw new Error('No id in parameters');
 
-        this._id = params.id;
+        const { id } = params;
         delete params.id;
 
-        super._init(params);
+        super(params);
 
+        this._id = id;
         let name = NetworksManager.getDefault().getNetworkName(this._id);
         this.name = `ConnectionRow ${name}`;
 
@@ -84,10 +85,10 @@ export const ConnectionsList = GObject.registerClass({
         'account-selected': {},
     },
 }, class ConnectionsList extends Gtk.ScrolledWindow {
-    _init(params) {
-        this._favoritesOnly = false;
+    _favoritesOnly = false;
 
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this.hscrollbar_policy = Gtk.PolicyType.NEVER;
 
@@ -282,15 +283,15 @@ export const ConnectionDetails = GObject.registerClass({
         'account-created': { param_types: [Tp.Account.$gtype] },
     },
 }, class ConnectionDetails extends Gtk.Grid {
-    _init(params) {
-        this._networksManager = NetworksManager.getDefault();
+    _networksManager = NetworksManager.getDefault();
+    _account = null;
+
+    constructor(params) {
+        super(params);
+
         let id = this._networksManager.connect('changed', () => {
             this.notify('has-service');
         });
-
-        this._account = null;
-
-        super._init(params);
 
         this.connect('destroy', () => {
             this._networksManager.disconnect(id);
@@ -508,9 +509,9 @@ export const ConnectionProperties = GObject.registerClass({
         'errorLabel',
     ],
 }, class ConnectionProperties extends Gtk.Dialog {
-    _init(account) {
+    constructor(account) {
         /* Translators: %s is a connection name */
-        super._init({
+        super({
             title: vprintf(_('“%s” Properties'), account.display_name),
             use_header_bar: 1,
         });

src/entryArea.js
@@ -28,8 +28,8 @@ export const ChatEntry = GObject.registerClass({
         'file-pasted': { param_types: [Gio.File.$gtype] },
     },
 }, class ChatEntry extends Gtk.Entry {
-    _init(params) {
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         const delegate = this.get_delegate();
         this.addTargets(delegate);
@@ -108,10 +108,10 @@ export const NickPopover = GObject.registerClass({
         'nick-changed': {},
     },
 }, class NickPopover extends Gtk.Popover {
-    _init() {
-        this._nick = '';
+    _nick = '';
 
-        super._init();
+    constructor() {
+        super();
 
         this.set_default_widget(this._changeButton);
 
@@ -166,16 +166,18 @@ export default GObject.registerClass({
         return this.__nickPopover;
     }
 
-    _init(params) {
-        this._room = params.room;
+    _maxNickChars = MAX_NICK_CHARS;
+    _nickChangedId = 0;
+    _popoverClosedId = 0;
+
+    constructor(params) {
+        const { room } = params;
         delete params.room;
 
-        this._ircParser = new IrcParser(this._room);
-        this._maxNickChars = MAX_NICK_CHARS;
-        this._nickChangedId = 0;
-        this._popoverClosedId = 0;
+        super(params);
 
-        super._init(params);
+        this._room = room;
+        this._ircParser = new IrcParser(this._room);
 
         this.connect('destroy', this._onDestroy.bind(this));
         this.connect('notify::sensitive', this._onSensitiveChanged.bind(this));

src/initialSetup.js
@@ -22,8 +22,8 @@ export default GObject.registerClass({
         'serverRoomList',
     ],
 }, class InitialSetupWindow extends Gtk.Window {
-    _init(params) {
-        super._init(params);
+    constructor(params) {
+        super(params);
 
         this._currentAccount = null;
 

commit 7c07e746a416c772985e3c8cc4dd1f25ae1833b9

userList: Use regular constructor
The UserList constructor was missed in commit 33368ebac when
switching to regular constructors.

Fixes: 33368ebac ("cleanup: Use regular constructors in GObject subclasses")
Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/343>

src/userList.js
@@ -528,8 +528,8 @@ class UserListRow extends Gtk.ListBoxRow {
 
 const UserList = GObject.registerClass(
 class UserList extends Gtk.ScrolledWindow {
-    _init(room) {
-        super._init({
+    constructor(room) {
+        super({
             hexpand: true,
             has_frame: true,
             hscrollbar_policy: Gtk.PolicyType.NEVER,