GNOME/polari

commit 32d149207e83cc94d979f525b2d2987b875b2ac7

connections: Use adaptive dialog for properties
Another fairly straight-forward case.

Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/310>

src/connections.js
@@ -508,7 +508,7 @@ class ConnectionDetails extends Adw.PreferencesPage {
 
 
 export const ConnectionProperties = GObject.registerClass(
-class ConnectionProperties extends Adw.Window {
+class ConnectionProperties extends Adw.Dialog {
     static [Gtk.template] = 'resource:///org/gnome/Polari/ui/connection-properties.ui';
     static [Gtk.internalChildren] = [
         'applyButton',
@@ -525,16 +525,6 @@ class ConnectionProperties extends Adw.Window {
         });
 
         this._details.account = account;
-
-        this._details.connect('notify::has-service', () => {
-            /* HACK:
-             * Shrink back to minimum height when the visibility of
-             * some elements in Details could have changed; this
-             * assumes that this only happens before the user could
-             * resize the dialog herself
-             */
-            this.resize(this.default_width, 1);
-        });
 
         this._applyButton.connect('clicked', () => {
             this._details.save();

data/resources/connection-properties.ui
@@ -7,27 +7,16 @@ SPDX-License-Identifier: GPL-2.0-or-later
 -->
 
 <interface>
-  <template class="Gjs_ConnectionProperties" parent="AdwWindow">
-    <property name="modal">True</property>
-    <property name="destroy-with-parent">True</property>
-    <property name="default-width">450</property>
+  <template class="Gjs_ConnectionProperties" parent="AdwDialog">
+    <property name="follows-content-size">true</property>
     <property name="default-widget">applyButton</property>
-    <child>
-      <object class="GtkShortcutController">
-        <child>
-          <object class="GtkShortcut">
-            <property name="trigger">Escape</property>
-            <property name="action">action(window.close)</property>
-          </object>
-        </child>
-      </object>
-    </child>
     <child>
       <object class="AdwToolbarView" id="view">
         <property name="bottom-bar-style">raised</property>
         <child type="top">
-          <object class="GtkHeaderBar">
-            <property name="show-title-buttons">False</property>
+          <object class="AdwHeaderBar">
+            <property name="show-start-title-buttons">False</property>
+            <property name="show-end-title-buttons">False</property>
             <child type="start">
               <object class="GtkButton" id="cancelButton">
                 <property name="label" translatable="yes">_Cancel</property>

src/application.js
@@ -790,9 +790,8 @@ class Application extends Adw.Application {
     _onEditConnection(action, parameter) {
         let accountPath = parameter.deep_unpack();
         let account = this._accountsMonitor.lookupAccount(accountPath);
-        let dialog = new Connections.ConnectionProperties(account);
-        dialog.transient_for = this.active_window;
-        dialog.show();
+        const dialog = new Connections.ConnectionProperties(account);
+        dialog.present(this.activeWindow);
     }
 
     _onStartClient() {

commit a32d32de427469afdffe9ca936dc21a5af27195d

connections: Do not destroy Adw.Dialog
Adw.Dialog aren't windows (and don't necessarily use one), so they
don't have the destroy() method, they are closed with close() instead.

Fixes: 32d149207 ("connections: Use adaptive dialog for properties")
Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/362>

src/connections.js
@@ -526,7 +526,7 @@ class ConnectionProperties extends Adw.Dialog {
 
         this._applyButton.connect('clicked', () => {
             this._details.save();
-            this.destroy();
+            this.close();
         });
 
         let id = account.connect('notify::connection-status',