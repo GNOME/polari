fbfacd945765deb9ac0e60646cdf62b1a73d67de
Carlos Garnacho <carlosg@gnome.org>
2025-10-28 20:14:58 +0100

    lib: Drop libpolari SparqlConnection singleton
    This is now managed entirely at JS code. There is no longer need
    for this helper.
    
    Part-of: <https://gitlab.gnome.org/GNOME/polari/-/merge_requests/365>

--- src/lib/polari-util.h
+++ src/lib/polari-util.h
@@ -19,7 +19,3 @@ gboolean polari_util_match_identify_message (const char  *message,
                                              char       **command,
                                              char       **username,
                                              char       **password);
-
-TrackerSparqlConnection *polari_util_get_tracker_connection (GError **error);
-
-void polari_util_close_tracker_connection (void);
